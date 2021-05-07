947a614527ff3019195fb1cf75be09c15687a466
ianbelfatto <ibelfatto@gmail.com>
2021-05-07 15:26:59 -0400

    adds url seg query, and body params for random number game and credentials

--- app/controllers/random_nums_controller.rb
+++ app/controllers/random_nums_controller.rb
@@ -2,6 +2,7 @@ class RandomNumsController < ApplicationController
   def randnum
     randnum = 36
     usernum = params[:usernum].to_i
+    
     if usernum < randnum
       render json: {
         message: "Too low!"
@@ -15,5 +16,18 @@ class RandomNumsController < ApplicationController
        message: "You got it!"
       }
     end
+  end
+
+  def body_credentials
+    username = params[:username]
+    password = params[:password]
+    output_message = ""
+
+    if username == "hugh" && password == "swordfish"
+      output_message = "valid"
+      else
+        output_message = "invalid"
+    end
+    render json: {message: output_message}
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/name", controller: "names", action: "name"
   get "/randnum", controller: "random_nums", action: "randnum"
+  get "/url_randnum/:usernum", controller: "random_nums", action: "randnum"
+
+  post "/body_guess", controller: "random_nums", action: "randnum"
+  post "/body_credentials", controller: "random_nums", action: "body_credentials"
 end
